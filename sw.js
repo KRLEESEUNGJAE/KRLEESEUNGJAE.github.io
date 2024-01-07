d68b7d9ed5fd4a1743b932d9635e70ca54fdd3ae
LEESEUNGJAE <karmaup0711@gmail.com>
2024-01-07 18:19:52 +0900

    feat: 댓글 기능 추가

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704617938';
+const cacheName = 'chirpy-1704619067';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
