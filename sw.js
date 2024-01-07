9ebe50ad3089bdb99d8912d49657da2987445080
LEESEUNGJAE <karmaup0711@gmail.com>
2024-01-08 00:00:24 +0900

    chore: 애널리틱스 수동 추가

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704639108';
+const cacheName = 'chirpy-1704639579';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
