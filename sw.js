9f101838a0611f2d93c2d4a04036228826efc08b
LEESEUNGJAE <karmaup0711@gmail.com>
2024-01-07 23:08:39 +0900

    fix: url 수정

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704620276';
+const cacheName = 'chirpy-1704636364';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
